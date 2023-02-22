c12344e6313238ef80a99741e99755b5a9b0fa5c
Timothy Sample <samplet@ngyro.com>
2023-02-22 20:37:47 +0100

    core: Add a C-only module lookup fast path.
    * include/mes/constants.h (MODULE_OBARRAY): New constant.
    (MODULE_USES): New constant.
    (MODULE_BINDER): New constant.
    * src/module.c (standard-eval-closure): New function.
    (standard_interface_eval_closure): New function.
    (module_make_local_var_x): New function.
    (module_variable): New function.
    (cuurent_module_variable): Use the new functions for lookup if the
    current module's eval-closure is one of two special symbols.
    * include/mes/mes.h: Declare new functions.
    * include/mes/symbols.h (cell_symbol_standard_eval_closure): New
    variable.
    (cell_symbol_standard_interface_eval_closure): New variable.
    (SYMBOL_MAX): Adjust accordingly.
    * src/symbol.c (init_symbols_): Initialize the new symbols.
    * mes/module/mes/guile-module.mes (standard-eval-closure)
    (standard-interface-eval-closure): Return a designated symbol
    instead of an actual closure.

--- include/mes/constants.h
+++ include/mes/constants.h
@@ -51,6 +51,9 @@
 #define GC_FRAME_SIZE 5
 #define GC_FRAME_PROCEDURE 4
 
+#define MODULE_OBARRAY 3
+#define MODULE_USES 4
+#define MODULE_BINDER 5
 #define MODULE_EVAL_CLOSURE 6
 
 #endif /* __MES_CONSTANTS_H */
