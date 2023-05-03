3dbc4bf135ecd3fbb4984f629ce73cd3ebe6c6bc
Janneke Nieuwenhuizen <janneke@gnu.org>
2023-05-03 22:04:37 +0200

    riscv64: lib: Use __init_io.
    * lib/linux/riscv64-mes-gcc/crt1.c (_start): Replace manual initialization
    of __stdin, __stdout, __stderr, and environ with __init_io ().
    * lib/linux/riscv64-mes-mescc/crt1.c (_start): Likewise.

--- lib/linux/riscv64-mes-gcc/crt1.c
+++ lib/linux/riscv64-mes-gcc/crt1.c
@@ -1,6 +1,6 @@
 /* -*-comment-start: "//";comment-end:""-*-
  * GNU Mes --- Maxwell Equations of Software
- * Copyright © 2017,2018,2019,2020 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2017,2018,2019,2020,2023 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
  * Copyright © 2019,2020 Danny Milosavljevic <dannym@scratchpost.org>
  * Copyright © 2021 W. J. van der Laan <laanwj@protonmail.com>
  *
@@ -21,7 +21,7 @@
  */
 
 #include <mes/lib-mini.h>
-//int main (int argc, char *argv[], char *envp[]);
+int main (int argc, char *argv[], char *envp[]);
 
 /* gcc will generate the following preamble:
 
@@ -42,42 +42,18 @@ _start ()
        "andi  sp, sp, ~15\n\t" // make sure sp conforms to ABI alignment
       );
 
-  asm (
-       "li    t0, 0\n\t"
-       "mv    %0, t0\n"
-       : "=r" (__stdin)
-       : //no inputs ""
-       );
-
-  asm (
-       "li    t0, 1\n\t"
-       "mv    %0, t0\n"
-       : "=r" (__stdout)
-       : //no inputs ""
-       );
-
-  asm (
-       "li    t0, 2\n\t"
-       "mv    %0, t0\n"
-       : "=r" (__stderr)
-       : //no inputs ""
-       );
-
   // environ is &argv[argc + 1]
   asm (
        "lw    t0, 0(s0)\n\t"
        "addi  t1, s0, 8\n\t"
        "addi  t0, t0, 1\n\t"
        "slli  t0, t0, 3\n\t"
-       "add   %0, t1, t0\n\t"
-       : "=r" (environ)
-       : //no inputs ""
-       );
+       "add   t0, t1, t0\n\t"
 
-  asm (
        "lw    a0, 0(s0)\n\t"  // a0 argc
        "addi  a1, s0, 8\n\t"  // a1 argv
-       "mv    a2, %0\n\t"     // a2 envp
+       "mv    a2, t0\n\t"     // a2 envp
+       "jal   __init_io\n\t"
        "jal   main\n\t"
 
        "li    a7, 93\n\t"     // SYS_exit
