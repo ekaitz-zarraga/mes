6f1b8b1dec6f7f5bdb2d2da937e789066309f7bd
Janneke Nieuwenhuizen <janneke@gnu.org>
2023-10-09 07:03:12 +0200

    squash! lib: Make stdarg work for GCC on RISC-V64.

--- include/stdarg.h
+++ include/stdarg.h
@@ -29,15 +29,15 @@
 
 #elif __GNUC__ && __riscv
 
-// GCC on RISC-V always passes arguments in registers. Implementing these macros without
-// the use of built-ins would be very involved.
+// GCC on RISC-V always passes arguments in registers.  Implementing
+// these macros without the use of built-ins would be very involved.
 typedef __builtin_va_list va_list;
 
-#define va_start(v,l)   __builtin_va_start(v,l)
-#define va_end(v)       __builtin_va_end(v)
-#define va_arg(v,l)     __builtin_va_arg(v,l)
-#define va_arg8(ap, type) va_arg(ap, type)
-#define va_copy(d,s)    __builtin_va_copy(d,s)
+#define va_start(v, l)     __builtin_va_start (v, l)
+#define va_end(v)          __builtin_va_end (v)
+#define va_arg(v, l)       __builtin_va_arg (v, l)
+#define va_arg8(ap, type)  va_arg (ap, type)
+#define va_copy(d, s)      __builtin_va_copy (d, s)
 
 #else // ! SYSTEM_LIBC && ! __riscv
 
