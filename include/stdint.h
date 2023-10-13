a5d3c0a21a93ece24e7b3fdf3db4534d085eb3b6
Ekaitz Zarraga <ekaitz@elenq.tech>
2023-10-13 21:31:20 +0200

    lib: int8_t set to `signed char` instead of `char`.
    `char` signedness is not defined by C standard.  In some architectures
    it's `unsigned` by default (RISC-V), so it's better to be explicit to
    avoid problems.
    
    * include/stdint.h (int8_t): Use `signed char` instead of `char`.

--- include/stdint.h
+++ include/stdint.h
@@ -50,7 +50,7 @@
 #undef ptrdiff_t
 
 typedef unsigned char uint8_t;
-typedef char int8_t;
+typedef signed char int8_t;
 typedef unsigned short uint16_t;
 typedef short int16_t;
 typedef unsigned uint32_t;
