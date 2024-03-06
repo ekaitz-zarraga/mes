bd27d8efb7d85b2828bd3703e324975173fadc49
Ekaitz Zarraga <ekaitz@elenq.tech>
2024-03-06 01:02:48 +0100

    configure: Make configure.sh print what it does...

--- configure.sh
+++ configure.sh
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e
+set -ex
 
 VERSION=0.26
 srcdir=${srcdir-$(dirname $0)}
