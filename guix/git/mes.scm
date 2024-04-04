802c7255b280c83d7b264b83b3e715a86c7fecee
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-04-04 23:55:47 +0200

    guix: mes: Update to use nyacc-1.07.4.
    * guix/git/mes.scm (mes)[propagated-inputs]: Use nyacc-1.07.4.

--- guix/git/mes.scm
+++ guix/git/mes.scm
@@ -335,7 +335,7 @@ $prefix/share/guile/site/$GUILE_EFFECTIVE_VERSION\n")))))
     (build-system gnu-build-system)
     (supported-systems '("aarch64-linux" "armhf-linux" "i686-linux"
                          "x86_64-linux" "riscv64-linux"))
-    (propagated-inputs (list mescc-tools nyacc-1.07.0))
+    (propagated-inputs (list mescc-tools nyacc-1.07.4))
     (native-inputs
      (append
       (list guile-3.0)
