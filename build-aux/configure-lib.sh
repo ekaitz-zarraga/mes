c6585fb0ceb1ed071f03101a33af7d99319ce420
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-05-02 01:48:31 +0200

    DRAFT lib: Implement popen, pclose.
    * lib/stub/pclose.c: Move to...
    * lib/stdio/pclose.c: ...here.  Implement.
    * lib/stub/popen.c: Move to...
    * lib/stdio/popen.c: ...here.  Implement.
    * build-aux/configure-lib.sh (libc_gnu_SOURCES): Update file locations.

--- build-aux/configure-lib.sh
+++ build-aux/configure-lib.sh
@@ -369,7 +369,9 @@ lib/stdio/feof.c
 lib/stdio/fgets.c
 lib/stdio/freopen.c
 lib/stdio/fscanf.c
+lib/stdio/pclose.c
 lib/stdio/perror.c
+lib/stdio/popen.c
 lib/stdio/rewind.c
 lib/stdio/vfscanf.c
 lib/stdlib/__exit.c
@@ -415,8 +417,6 @@ lib/stub/gmtime.c
 lib/stub/log.c
 lib/stub/mktime.c
 lib/stub/modf.c
-lib/stub/pclose.c
-lib/stub/popen.c
 lib/stub/pow.c
 lib/stub/rand.c
 lib/stub/setbuf.c
